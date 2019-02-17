9f22a18afeff193095adbd3e8f477e71a5d4a9e5
Bora Shin <42104631+borazvim@users.noreply.github.com>
2019-02-17 23:31:43 +0900

    Create (11053 dynamic programming)가장긴수열.cpp
    #include <vector>
    #include <algorithm>
    vector<int> a(n);
    *max_element(d.begin(), d.end())

---
+++ (11053 dynamic programming)가장긴수열.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	vector<int> a(n);
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+	vector<int> d(n);
+	for (int i = 0; i < n; i++) {
+		d[i] = 1;
+		for (int j = 0; j < i; j++) {
+			if (a[j]<a[i] && d[j] + 1>d[i])
+				d[i] = d[j] + 1;
+		}
+	}
+	cout << *max_element(d.begin(), d.end()) << '\n';
+}
